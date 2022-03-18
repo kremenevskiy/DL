5c52cc875299cf107ca75ecdcdcbcaa9d056f87e
kremenevskiy <kremeneuski@gmail.com>
2022-03-18 03:20:25 +0300

    implementation of dropout layer

--- CS231n/cs231n/assignments/assignment2/cs231n/layers.py
+++ CS231n/cs231n/assignments/assignment2/cs231n/layers.py
@@ -454,6 +454,7 @@ def layernorm_backward(dout, cache):
 
 
 def dropout_forward(x, dropout_param):
+    
     """Forward pass for inverted dropout.
 
     Note that this is different from the vanilla version of dropout.
@@ -490,7 +491,9 @@ def dropout_forward(x, dropout_param):
         #######################################################################
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-        pass
+        mask = (np.random.rand(*x.shape) < p) / p
+        out = x * mask
+
 
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         #######################################################################
@@ -502,7 +505,7 @@ def dropout_forward(x, dropout_param):
         #######################################################################
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-        pass
+        out = x
 
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         #######################################################################
@@ -532,7 +535,7 @@ def dropout_backward(dout, cache):
         #######################################################################
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-        pass
+        dx = dout * mask
 
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         #######################################################################
